55dcc550c66ac672a6ec9b06524ce4a58946691e
irineu <mmmirineusilva@gmail.com>
2025-06-15 03:56:58 -0300

    Implement svg support

--- composeApp/src/commonMain/kotlin/com/neoutils/image/App.kt
+++ composeApp/src/commonMain/kotlin/com/neoutils/image/App.kt
@@ -3,16 +3,24 @@ package com.neoutils.image
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.text.BasicText
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.geometry.isSpecified
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asComposeImageBitmap
 import androidx.compose.ui.graphics.decodeToImageBitmap
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Density
 import com.neoutils.image.resources.Res
-import com.neoutils.image.resources.crazy_cat
+import com.neoutils.image.resources.atom
 import io.ktor.client.*
 import io.ktor.client.plugins.*
 import io.ktor.client.request.*
@@ -30,6 +38,8 @@ import org.jetbrains.compose.resources.rememberResourceEnvironment
 import org.jetbrains.skia.Bitmap
 import org.jetbrains.skia.Codec
 import org.jetbrains.skia.Data
+import org.jetbrains.skia.Rect
+import org.jetbrains.skia.svg.*
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
 
@@ -38,7 +48,7 @@ fun App() = Box(
     contentAlignment = Alignment.Center,
     modifier = Modifier.fillMaxSize()
 ) {
-    val resource = asyncImageBitmapResource(res = Res.drawable.crazy_cat)
+    val resource = asyncPainterResource(res = Res.drawable.atom)
 
     when (resource) {
         is Resource.Loading -> {
@@ -50,12 +60,12 @@ fun App() = Box(
         }
 
         is Resource.Result.Failure -> {
-            BasicText(text = "error: ${resource.exception.message}")
+            throw resource.exception
         }
 
-        is Resource.Result.Success<ImageBitmap> -> {
+        is Resource.Result.Success<Painter> -> {
             Image(
-                bitmap = resource.data,
+                painter = resource.data,
                 contentDescription = null
             )
         }
@@ -96,11 +106,15 @@ sealed interface Image {
     class Animated(
         val codec: Codec,
     ) : Image
+
+    class Vector(
+        val svgDom: SVGDOM
+    ) : Image
 }
 
 private val DefaultAnimationDuration = 100.milliseconds
 
-fun Image.Animated.animate() = callbackFlow {
+fun Image.Animated.animateImageBitmap() = callbackFlow {
 
     val bitmap = Bitmap().apply {
         allocPixels(codec.imageInfo)
@@ -204,6 +218,13 @@ class ImageFromResources(
 fun ByteArray.toImage(): Image {
 
     val data = Data.makeFromBytes(this)
+
+    runCatching {
+        Image.Vector(SVGDOM(data))
+    }.onSuccess {
+        return it
+    }
+
     val codec = Codec.makeFromData(data)
 
     return if (codec.frameCount == 1) {
@@ -218,10 +239,10 @@ fun ByteArray.toImage(): Image {
 }
 
 @Composable
-fun asyncImageBitmapResource(
+fun asyncPainterResource(
     url: String,
     imageFromUrl: ImageFromUrl = remember { ImageFromUrl() }
-): Resource<ImageBitmap> {
+): Resource<Painter> {
 
     val imageFlow = remember(imageFromUrl, url) { imageFromUrl.fetch(url) }
 
@@ -229,8 +250,9 @@ fun asyncImageBitmapResource(
 
     return resource.mapSuccess {
         when (it) {
-            is Image.Animated -> it.animatedImageBitmap()
-            is Image.Static -> it.bitmap
+            is Image.Animated -> BitmapPainter(it.animatedImageBitmap())
+            is Image.Static -> BitmapPainter(it.bitmap)
+            is Image.Vector -> SvgPainter(it.svgDom, LocalDensity.current)
         }
     }
 }
@@ -241,10 +263,10 @@ fun rememberImageFromResources(
 ) = remember(environment) { ImageFromResources(environment) }
 
 @Composable
-fun asyncImageBitmapResource(
+fun asyncPainterResource(
     res: DrawableResource,
     imageFromResources: ImageFromResources = rememberImageFromResources()
-): Resource<ImageBitmap> {
+): Resource<Painter> {
 
     var imageResource by remember { mutableStateOf<Resource<Image>>(Resource.Loading()) }
 
@@ -254,8 +276,9 @@ fun asyncImageBitmapResource(
 
     return imageResource.mapSuccess {
         when (it) {
-            is Image.Animated -> it.animatedImageBitmap()
-            is Image.Static -> it.bitmap
+            is Image.Animated -> BitmapPainter(it.animatedImageBitmap())
+            is Image.Static -> BitmapPainter(it.bitmap)
+            is Image.Vector -> SvgPainter(it.svgDom, LocalDensity.current)
         }
     }
 }
@@ -265,9 +288,53 @@ private val ImageBlank = ImageBitmap(1, 1)
 @Composable
 fun Image.Animated.animatedImageBitmap(): ImageBitmap {
 
-    val animationFlow = remember { animate() }
+    val animationFlow = remember { animateImageBitmap() }
 
     return animationFlow.collectAsState(
         initial = ImageBlank
     ).value
+}
+
+/**
+ * Copied from Compose Resources
+ */
+class SvgPainter(
+    private val dom: SVGDOM,
+    private val density: Density
+) : Painter() {
+    private val svg = dom.root
+
+    private val defaultSizePx: Size = run {
+        val width = svg?.width?.withUnit(SVGLengthUnit.PX)?.value ?: 0f
+        val height = svg?.height?.withUnit(SVGLengthUnit.PX)?.value ?: 0f
+        if (width == 0f && height == 0f) {
+            Size.Unspecified
+        } else {
+            Size(width, height)
+        }
+    }
+
+    init {
+        if (svg?.viewBox == null && defaultSizePx.isSpecified) {
+            svg?.viewBox = Rect.makeXYWH(0f, 0f, defaultSizePx.width, defaultSizePx.height)
+        }
+    }
+
+    override val intrinsicSize: Size
+        get() {
+            return if (defaultSizePx.isSpecified) {
+                defaultSizePx * density.density
+            } else {
+                Size.Unspecified
+            }
+        }
+
+    override fun DrawScope.onDraw() {
+        drawIntoCanvas { canvas ->
+            svg?.width = SVGLength(size.width, SVGLengthUnit.PX)
+            svg?.height = SVGLength(size.height, SVGLengthUnit.PX)
+            svg?.preserveAspectRatio = SVGPreserveAspectRatio(SVGPreserveAspectRatioAlign.NONE)
+            dom.render(canvas.nativeCanvas)
+        }
+    }
 }
