073a09f79f7f76a70e591a80ed4601d02e145a19
irineu <mmmirineusilva@gmail.com>
2025-06-28 03:57:59 -0300

    chore(cache): create MemoryCacheInterceptor (wip)

--- nil-core/src/commonMain/kotlin/com/neoutils/nil/core/source/Interceptor.kt
+++ nil-core/src/commonMain/kotlin/com/neoutils/nil/core/source/Interceptor.kt
@@ -3,9 +3,9 @@ package com.neoutils.nil.core.source
 import androidx.compose.runtime.compositionLocalOf
 import com.neoutils.nil.core.interceptor.DecodeInterceptor
 import com.neoutils.nil.core.interceptor.FetchInterceptor
-import com.neoutils.nil.core.util.Level
 import com.neoutils.nil.core.model.Chain
 import com.neoutils.nil.core.model.Settings
+import com.neoutils.nil.core.util.Level
 import kotlinx.coroutines.flow.Flow
 
 val LocalInterceptors = compositionLocalOf {
--- nil-core/src/commonMain/kotlin/com/neoutils/nil/core/util/Resource.kt
+++ nil-core/src/commonMain/kotlin/com/neoutils/nil/core/util/Resource.kt
@@ -1,19 +1,31 @@
 package com.neoutils.nil.core.util
 
-sealed interface Resource<out T> {
+sealed interface Resource<out T> : State {
 
     data class Loading(
         val progress: Float? = null
-    ) : Resource<Nothing>
+    ) : Resource<Nothing> {
+        override val isLoading = true
+        override val isSuccess = false
+        override val isFailure = false
+    }
 
     sealed interface Result<out T> : Resource<T> {
 
         data class Success<out T>(
             val value: T
-        ) : Result<T>
+        ) : Result<T> {
+            override val isLoading = false
+            override val isSuccess = true
+            override val isFailure = false
+        }
 
         data class Failure(
             val throwable: Throwable
-        ) : Result<Nothing>
+        ) : Result<Nothing> {
+            override val isLoading = false
+            override val isSuccess = false
+            override val isFailure = true
+        }
     }
 }
