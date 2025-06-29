ee31755c336838315be65bffbf435b426adcb33e
irineu <mmmirineusilva@gmail.com>
2025-06-29 10:56:19 -0300

    chore(fetchers): load defaults dynamically

--- nil-core/src/commonMain/kotlin/com/neoutils/nil/core/util/Dynamic.kt
+++ nil-core/src/commonMain/kotlin/com/neoutils/nil/core/util/Dynamic.kt
@@ -1,10 +1,12 @@
 package com.neoutils.nil.core.util
 
 import com.neoutils.nil.core.source.Decoder
+import com.neoutils.nil.core.source.Fetcher
 import java.util.*
 
 object Dynamic {
     val decoders = load<Decoder>()
+    val fetchers = load<Fetcher<*>>()
 }
 
 private inline fun <reified T : Any> load(): List<T> = ServiceLoader.load(
