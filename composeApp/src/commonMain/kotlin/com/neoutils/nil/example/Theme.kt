06a41d6b048c0aa199084e2a9bf0efe7df11e3f0
irineu <mmmirineusilva@gmail.com>
2025-06-29 10:56:19 -0300

    chore(fetchers): load defaults dynamically

--- composeApp/src/commonMain/kotlin/com/neoutils/nil/example/Theme.kt
+++ composeApp/src/commonMain/kotlin/com/neoutils/nil/example/Theme.kt
@@ -4,22 +4,11 @@ import androidx.compose.runtime.Composable
 import com.neoutils.nil.core.composable.ProvideSettings
 import com.neoutils.nil.core.composable.rememberSettings
 import com.neoutils.nil.fetcher.network.extension.network
-import com.neoutils.nil.fetcher.network.impl.NetworkFetcher
-import com.neoutils.nil.fetcher.resources.impl.ResourcesFetcher
-import com.neoutils.nil.interceptor.memoryCache.MemoryCacheInterceptor
 
 @Composable
 fun AppTheme(content: @Composable () -> Unit) {
     ProvideSettings(
         settings = rememberSettings {
-
-            fetchers = listOf(
-                NetworkFetcher(),
-                ResourcesFetcher()
-            )
-
-            interceptors += MemoryCacheInterceptor()
-
             extras {
                 network {
                     headers = mapOf(
