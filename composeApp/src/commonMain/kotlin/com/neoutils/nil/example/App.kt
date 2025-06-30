7e3a4afb1aaa5278f3e3f07601f45a83a3201b05
irineu <mmmirineusilva@gmail.com>
2025-06-30 18:37:59 -0300

    chore(core): create AsyncImage

--- composeApp/src/commonMain/kotlin/com/neoutils/nil/example/App.kt
+++ composeApp/src/commonMain/kotlin/com/neoutils/nil/example/App.kt
@@ -1,14 +1,11 @@
 package com.neoutils.nil.example
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.neoutils.nil.core.composable.asyncPainterResource
-import com.neoutils.nil.core.util.PainterResource
+import com.neoutils.nil.core.composable.AsyncImage
 import com.neoutils.nil.core.util.Request
 import com.neoutils.nil.decoder.gif.extension.gif
 import com.neoutils.nil.decoder.svg.extension.svg
@@ -21,38 +18,15 @@ fun App() = AppTheme {
         contentAlignment = Alignment.Center,
         modifier = Modifier.fillMaxSize()
     ) {
-
-        val resource = asyncPainterResource(
+        AsyncImage(
             request = Request.network("https://cataas.com/cat"),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize()
         ) {
             decoders {
                 gif()
                 xml()
                 svg()
-            }
-        }
-
-        when (resource) {
-            is PainterResource.Result.Success -> {
-                Image(
-                    painter = resource,
-                    contentDescription = null,
-                    modifier = Modifier.fillMaxSize()
-                )
-            }
-
-            is PainterResource.Loading -> {
-                if (resource.progress != null) {
-                    CircularProgressIndicator(
-                        progress = { resource.progress!! }
-                    )
-                } else {
-                    CircularProgressIndicator()
-                }
-            }
-
-            is PainterResource.Result.Failure -> {
-                throw resource.throwable
             }
         }
     }
