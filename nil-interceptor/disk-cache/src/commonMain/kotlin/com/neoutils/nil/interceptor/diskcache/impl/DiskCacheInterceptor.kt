057f380b330e2f582a43b96d17f045cd655426fa
irineu <mmmirineusilva@gmail.com>
2025-07-02 07:10:16 -0300

    chore(DiskCacheInterceptor): dynamic load

--- nil-interceptor/disk-cache/src/commonMain/kotlin/com/neoutils/nil/interceptor/diskcache/impl/DiskCacheInterceptor.kt
+++ nil-interceptor/disk-cache/src/commonMain/kotlin/com/neoutils/nil/interceptor/diskcache/impl/DiskCacheInterceptor.kt
@@ -1,4 +1,4 @@
-package com.neoutils.nil.interceptor.diskcache
+package com.neoutils.nil.interceptor.diskcache.impl
 
 import com.neoutils.nil.core.extension.onSuccess
 import com.neoutils.nil.core.model.Chain
@@ -6,19 +6,18 @@ import com.neoutils.nil.core.model.Settings
 import com.neoutils.nil.core.source.Interceptor
 import com.neoutils.nil.core.util.*
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import okio.FileSystem
 
 private const val DefaultCachePath = "nil-cache"
 
-val DiskCacheExtrasKey = Extras.Key(
+val DiskCachePathExtrasKey = Extras.Key(
     default = FileSystem.SYSTEM_TEMPORARY_DIRECTORY / DefaultCachePath
 )
 
 class DiskCacheInterceptor : Interceptor(Level.REQUEST, Level.DATA) {
 
-    private val fileSystem = FileSystem.SYSTEM
+    private val fileSystem by lazy { FileSystem.SYSTEM }
 
     private val Request.key
         get() = when (this) {
@@ -33,22 +32,21 @@ class DiskCacheInterceptor : Interceptor(Level.REQUEST, Level.DATA) {
 
         val key = chain.request.key ?: return flowOf(chain)
 
-        val path = settings.extras[DiskCacheExtrasKey]
+        val path = settings.extras[DiskCachePathExtrasKey]
 
         val file = path / key.clear()
 
         if (fileSystem.exists(file)) {
-            return flow {
-                val bytes = fileSystem.read(file) {
-                    readByteArray()
-                }
 
-                emit(
-                    chain.copy(
-                        data = Resource.Result.Success(bytes)
-                    )
-                )
+            val bytes = fileSystem.read(file) {
+                readByteArray()
             }
+
+            return flowOf(
+                chain.copy(
+                    data = Resource.Result.Success(bytes)
+                )
+            )
         }
 
         chain.data.onSuccess { chain ->
