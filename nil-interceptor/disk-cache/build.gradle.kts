3c890c04a293e236117189f5299eb3c384cba18d
irineu <mmmirineusilva@gmail.com>
2025-07-26 12:06:55 -0300

    chore(cache): implement ios support

--- nil-interceptor/disk-cache/build.gradle.kts
+++ nil-interceptor/disk-cache/build.gradle.kts
@@ -20,6 +20,10 @@ kotlin {
         }
     }
 
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
     sourceSets {
         commonMain.dependencies {
 
---
+++ nil-interceptor/memory-cache/src/iosMain/kotlin/com/neoutils/nil/interceptor/memoryCache/Dynamic.kt
@@ -0,0 +1,11 @@
+@file:OptIn(ExperimentalStdlibApi::class)
+
+package com.neoutils.nil.interceptor.memoryCache
+
+import com.neoutils.nil.core.util.interceptors
+import com.neoutils.nil.interceptor.memoryCache.impl.MemoryCacheInterceptor
+
+@EagerInitialization
+private val initHook = {
+    interceptors.add(MemoryCacheInterceptor())
+}.invoke()
