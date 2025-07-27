1fac8c116533dabb9c8fc23e94ab55ee2cd0fbee
irineu <mmmirineusilva@gmail.com>
2025-07-27 19:54:41 -0300

    chore: improve preview

--- nil-fetcher/resources/src/androidMain/kotlin/com/neoutils/nil/fetcher/resources/compose/ResourcesPreview.android.kt
+++ nil-fetcher/resources/src/androidMain/kotlin/com/neoutils/nil/fetcher/resources/compose/ResourcesPreview.android.kt
@@ -3,12 +3,16 @@ package com.neoutils.nil.fetcher.resources.compose
 import androidx.compose.runtime.Composable
 import com.neoutils.nil.core.composable.ProvideSettings
 import com.neoutils.nil.core.model.Settings
+import org.jetbrains.compose.resources.PreviewContextConfigurationEffect
 
 @Composable
 actual fun ResourcesPreview(
     settings: Settings,
-    content: @Composable () -> Unit
+    content: @Composable (() -> Unit)
 ) {
+    // required to compose resources
+    PreviewContextConfigurationEffect()
+
     ProvideSettings(
         settings = settings,
         content = content,
