524ebbccb340945a33abbba060343233a31cddef
irineu <mmmirineusilva@gmail.com>
2025-08-26 01:50:40 -0300

    chore(gif & svg): improve platform separation

--- nil-decoder/gif/src/desktopMain/kotlin/com/neoutils/nil/decoder/gif/di/Platform.desktop.kt
+++
@@ -1,6 +0,0 @@
-package com.neoutils.nil.decoder.gif.di
-
-import com.neoutils.nil.core.foundation.Decoder
-import com.neoutils.nil.decoder.gif.impl.SkiaGifDecoder
-
-internal actual val platformDecoder: Decoder get() = SkiaGifDecoder()
--- nil-decoder/svg/src/desktopMain/kotlin/com/neoutils/nil/decoder/svg/di/Platform.kt
+++
@@ -1,6 +0,0 @@
-package com.neoutils.nil.decoder.svg.di
-
-import com.neoutils.nil.core.foundation.Decoder
-import com.neoutils.nil.decoder.svg.impl.SkiaSvgDecoder
-
-internal actual val platformDecoder: Decoder get() = SkiaSvgDecoder()
