3d86f12227bbb2c771bdb2067b6102b1fe9006ad
irineu <mmmirineusilva@gmail.com>
2025-08-28 00:35:06 -0300

    doc(README): improve quick start

---
+++ nil-fetcher/network-default/build.gradle.kts
@@ -0,0 +1,137 @@
+@file:OptIn(ExperimentalWasmDsl::class)
+
+import com.vanniktech.maven.publish.KotlinMultiplatform
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.maven.publish)
+}
+
+kotlin {
+    jvm("desktop") {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_11
+        }
+    }
+
+    androidTarget {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_11
+        }
+    }
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+
+        commonMain {
+            dependencies {
+
+                // module
+                api(project(":nil-fetcher:network"))
+
+                // compose
+                implementation(compose.runtime)
+            }
+        }
+
+        androidMain {
+            dependencies {
+                implementation(libs.ktor.client.android)
+            }
+        }
+
+        iosArm64Main {
+            dependencies {
+                implementation(libs.ktor.client.darwin)
+            }
+        }
+
+        val desktopMain by getting {
+            dependencies {
+                implementation(libs.ktor.client.java)
+            }
+        }
+    }
+}
+
+android {
+    namespace = "com.neoutils.nil.fecther.network"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+mavenPublishing {
+
+    publishToMavenCentral()
+
+    coordinates(
+        artifactId = "network-fetcher-default",
+        groupId = "com.neoutils.nil",
+        version = "0.1.0-alpha04"
+    )
+
+    configure(
+        KotlinMultiplatform(
+            sourcesJar = true,
+            androidVariantsToPublish = listOf("release"),
+        )
+    )
+
+    pom {
+        name = "network-fetcher-default"
+        description = "Ktor fetcher with engines for Nil."
+        inceptionYear = "2025"
+        url = "https://github.com/Irineu333/ImageLoader-POC"
+
+        licenses {
+            license {
+                name = "The MIT License"
+                url = "https://opensource.org/licenses/MIT"
+            }
+        }
+
+        developers {
+            developer {
+                id = "irineu333"
+                name = "Irineu A. Silva"
+                url = "https://github.com/Irineu333"
+            }
+        }
+
+        scm {
+            url = "https://github.com/Irineu333/ImageLoader-POC"
+            connection = "scm:git:git://github.com/Irineu333/ImageLoader-POC.git"
+            developerConnection = "scm:git:ssh://git@github.com/Irineu333/ImageLoader-POC.git"
+        }
+    }
+
+    signAllPublications()
+}
